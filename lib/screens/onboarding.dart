94d1e526a9c78a912c0b2705966a86602995ac30
Juan <jdeluca@itba.edu.ar>
2021-10-01 00:28:31 -0300

    onboarding buttons

--- lib/screens/onboarding.dart
+++ lib/screens/onboarding.dart
@@ -48,93 +48,49 @@ class Onboarding extends StatelessWidget {
                     ),
                   ],
                 ),
-                Container(
-                  
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 16.0),
-                  // decoration: BoxDecoration(
-                  //   border: Border.all(color: ArgonColors.white, width: 2),
-                  // ),
-                  child: SizedBox(
-                    width: double.infinity,
-                    child: FlatButton(
-                      textColor: ArgonColors.white,
-                      color: ArgonColors.primary,
-                      onPressed: () {
-                        Navigator.pushReplacementNamed(context, '/home');
-                      },
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(4.0),
-                      ),
-                      child: Padding(
-                          padding: EdgeInsets.only(
-                              left: 16.0, right: 16.0, top: 12, bottom: 12),
-                          child: Text("Iniciar Sesión",
-                              style: TextStyle(
-                                  fontWeight: FontWeight.w600,
-                                  fontSize: 16.0))),
+                TextButton(
+                    child: SizedBox(
+                        width: double.infinity,
+                        child: Text(
+                            "Iniciar Sesión",
+                            style: TextStyle(fontSize: 20),
+                            textAlign: TextAlign.center
+                        )
                     ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 10.0),
-                  child: SizedBox(
-                    width: double.infinity,
-                    child: FlatButton(
-                      textColor: ArgonColors.black,
-                      color: ArgonColors.white,
-                      onPressed: () {
-                        Navigator.pushReplacementNamed(context, '/home');
-                      },
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(4.0),
-                      ),
-                      child: Padding(
-                          padding: EdgeInsets.only(
-                              left: 16.0, right: 16.0, top: 12, bottom: 12),
-                          child: Text("Registrarse",
-                              style: TextStyle(
-                                  fontWeight: FontWeight.w600,
-                                  fontSize: 16.0))),
+                    style: ButtonStyle(
+                        padding: MaterialStateProperty.all<EdgeInsets>(EdgeInsets.all(15)),
+                        foregroundColor: MaterialStateProperty.all<Color>(ArgonColors.white),
+                        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(18.0),
+                                side: BorderSide(color: ArgonColors.white, width: 1.5)
+                            )
+                        )
                     ),
-                  ),
+                    onPressed: () => null
                 ),
-                // Container(
-                //   decoration: BoxDecoration(
-                //     border: Border.all(color: Colors.blue, width: 4),
-                //     color: Colors.yellow,
-                //     shape: BoxShape.circle,
-                //   ),
-                //   child: IconButton(
-                //     iconSize: 56,
-                //     icon: Icon(Icons.check),
-                //     onPressed: () {},
-                //   ),
-                // ),
-                // Material(
-                //   type: MaterialType.transparency, //Makes it usable on any background color, thanks @IanSmith
-                //   child: Ink(
-                //     decoration: BoxDecoration(
-                //       border: Border.all(color: Colors.indigoAccent, width: 4.0),
-                //       color: Colors.indigo[900],
-                //       shape: BoxShape.circle,
-                //     ),
-                //     child: InkWell(
-                //       //This keeps the splash effect within the circle
-                //       borderRadius: BorderRadius.circular(1000.0), //Something large to ensure a circle
-                //       onTap: _messages,
-                //       child: Padding(
-                //         padding:EdgeInsets.all(20.0),
-                //         child: Icon(
-                //           Icons.message,
-                //           size: 30.0,
-                //           color: Colors.white,
-                //         ),
-                //       ),
-                //     ),
-                //   )
-                // ),
+                TextButton(
+                    child: SizedBox(
+                      width: double.infinity,
+                      child: Text(
+                          "Registrarse",
+                          style: TextStyle(fontSize: 20),
+                          textAlign: TextAlign.center
+                      )
+                    ),
+                    style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(ArgonColors.white),
+                        padding: MaterialStateProperty.all<EdgeInsets>(EdgeInsets.all(15)),
+                        foregroundColor: MaterialStateProperty.all<Color>(ArgonColors.black),
+                        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(18.0),
+                                // side: BorderSide(color: ArgonColors.white, width: 1.5)
+                            )
+                        )
+                    ),
+                    onPressed: () => null
+                ),
                 // Row(
                 //   mainAxisAlignment: MainAxisAlignment.center,
                 //   image:DecorationImage(
