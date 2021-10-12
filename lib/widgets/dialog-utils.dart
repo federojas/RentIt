a62ff025a259c1baa5bf649235609a3f499e607e
Santiago Sandrini <ssandrini@itba.edu.ar>
2021-10-11 21:33:53 -0300

    Algunos cambios

---
+++ lib/widgets/dialog-utils.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class DialogUtils {
+  static DialogUtils _instance = new DialogUtils.internal();
+
+  DialogUtils.internal();
+
+  factory DialogUtils() => _instance;
+
+  static void showCustomDialog(BuildContext context,
+      {@required String title,
+        String okBtnText = "Ok",
+        String cancelBtnText = "Cancel",
+        @required Function okBtnFunction}) {
+    showDialog(
+        context: context,
+        builder: (_) {
+          return AlertDialog(
+            title: Text(title),
+            content: /* Here add your custom widget  */,
+            actions: <Widget>[
+              FlatButton(
+                child: Text(okBtnText),
+                onPressed: okBtnFunction,
+              ),
+              FlatButton(
+                  child: Text(cancelBtnText),
+                  onPressed: () => Navigator.pop(context))
+            ],
+          );
+        });
+  }
+}
