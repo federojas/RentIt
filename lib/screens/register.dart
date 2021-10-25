e1306dc13def6adc3feae9f83aa8ef405a51a1b7
Santiago Sandrini <ssandrini@itba.edu.ar>
2021-10-25 09:52:54 -0300

    Nuevo register

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -6,6 +6,236 @@ import 'package:argon_flutter/widgets/input.dart';
 import 'package:argon_flutter/net/flutterfire.dart';
 
 class Register extends StatefulWidget {
+  const Register({Key key}) : super(key: key);
+
+  @override
+  _RegisterState createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+
+  final _formKey = GlobalKey<FormState>();
+
+  final firstNameEditingController = new TextEditingController();
+  final lastNameEditingController = new TextEditingController();
+  final emailEditingController = new TextEditingController();
+  final passwordEditingController = new TextEditingController();
+  final confirmPasswordEditingController = new TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+
+    final firstNameField = TextFormField(
+        autofocus: false,
+        controller: firstNameEditingController,
+        keyboardType: TextInputType.name,
+        validator: (value) {
+          RegExp regex = new RegExp(r'^.{3,}$');
+          if (value.isEmpty) {
+            return ("No puede ser vacío");
+          }
+          if (!regex.hasMatch(value)) {
+            return ("Mínimo 3 caracteres");
+          }
+          return null;
+        },
+        onSaved: (value) {
+          firstNameEditingController.text = value;
+        },
+        textInputAction: TextInputAction.next,
+        decoration: InputDecoration(
+          prefixIcon: Icon(Icons.account_circle),
+          contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+          hintText: "Nombre",
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ));
+
+    //second name field
+    final lastNameField = TextFormField(
+        autofocus: false,
+        controller: lastNameEditingController,
+        keyboardType: TextInputType.name,
+        validator: (value) {
+          if (value.isEmpty) {
+            return ("No puede ser vacío");
+          }
+          return null;
+        },
+        onSaved: (value) {
+          lastNameEditingController.text = value;
+        },
+        textInputAction: TextInputAction.next,
+        decoration: InputDecoration(
+          prefixIcon: Icon(Icons.account_circle),
+          contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+          hintText: "Apellido",
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ));
+
+    //email field
+    final emailField = TextFormField(
+        autofocus: false,
+        controller: emailEditingController,
+        keyboardType: TextInputType.emailAddress,
+        validator: (value) {
+          if (value.isEmpty) {
+            return ("Ingrese un email válido");
+          }
+          // reg expression for email validation
+          if (!RegExp("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-z]")
+              .hasMatch(value)) {
+            return ("Ingrese un email válido");
+          }
+          return null;
+        },
+        onSaved: (value) {
+          firstNameEditingController.text = value;
+        },
+        textInputAction: TextInputAction.next,
+        decoration: InputDecoration(
+          prefixIcon: Icon(Icons.mail),
+          contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+          hintText: "Email",
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ));
+
+    //password field
+    final passwordField = TextFormField(
+        autofocus: false,
+        controller: passwordEditingController,
+        obscureText: true,
+        validator: (value) {
+          RegExp regex = new RegExp(r'^.{6,}$');
+          if (value.isEmpty) {
+            return ("Este campo es requerido");
+          }
+          else if (!regex.hasMatch(value)) {
+            return ("Mínimo 6 caracteres");
+          }
+          return "";
+        },
+        onSaved: (value) {
+          firstNameEditingController.text = value;
+        },
+        textInputAction: TextInputAction.next,
+        decoration: InputDecoration(
+          prefixIcon: Icon(Icons.vpn_key),
+          contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+          hintText: "Contraseña",
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ));
+
+    //confirm password field
+    final confirmPasswordField = TextFormField(
+        autofocus: false,
+        controller: confirmPasswordEditingController,
+        obscureText: true,
+        validator: (value) {
+          if (confirmPasswordEditingController.text !=
+              passwordEditingController.text) {
+            return "Las contraseñas no coinciden";
+          }
+          return null;
+        },
+        onSaved: (value) {
+          confirmPasswordEditingController.text = value;
+        },
+        textInputAction: TextInputAction.done,
+        decoration: InputDecoration(
+          prefixIcon: Icon(Icons.vpn_key),
+          contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+          hintText: "Confirmar contraseña",
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ));
+
+    //signup button
+    final signUpButton = Material(
+      elevation: 5,
+      borderRadius: BorderRadius.circular(30),
+      color: MyTheme.primary,
+      child: MaterialButton(
+          padding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+          minWidth: MediaQuery
+              .of(context)
+              .size
+              .width,
+          onPressed: () {
+            register(emailEditingController.text, passwordEditingController.text);
+          },
+          child: Text(
+            "Registrarse",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold),
+          )),
+    );
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.red),
+          onPressed: () {
+            // passing this to our root
+            Navigator.pushReplacementNamed(context, '/onboarding');
+          },
+        ),
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Container(
+            color: Colors.white,
+            child: Padding(
+              padding: const EdgeInsets.all(36.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    SizedBox(
+                        height: 180,
+                        child: Image.asset(
+                          "assets/img/rentItLogo.png",
+                          fit: BoxFit.contain,
+                        )),
+                    SizedBox(height: 45),
+                    firstNameField,
+                    SizedBox(height: 20),
+                    lastNameField,
+                    SizedBox(height: 20),
+                    emailField,
+                    SizedBox(height: 20),
+                    passwordField,
+                    SizedBox(height: 20),
+                    confirmPasswordField,
+                    SizedBox(height: 20),
+                    signUpButton,
+                    SizedBox(height: 15),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+/*
+class Register extends StatefulWidget {
   @override
   _RegisterState createState() => _RegisterState();
 }
@@ -22,175 +252,90 @@ class _RegisterState extends State<Register> {
       resizeToAvoidBottomInset: false,
       backgroundColor: MyTheme.secondary,
       body: Center(
-          child: Container(
-        width: MediaQuery.of(context).size.width / 1.3,
-        height: MediaQuery.of(context).size.height / 1.2,
-        margin: const EdgeInsets.all(10.0),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(16),
-          color: MyTheme.primary,
-        ),
-        child:
-            Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-          Container(
-            child: Center(
-              child: Image.asset("assets/img/rentItLogo.png",
-                  height: 200, width: 200),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 16.0, right: 16.0),
-            child: RegisterInput(
-              placeholder: "Usuario",
-              controller: _userField,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 16.0, right: 16.0),
-            child: RegisterInput(
-              placeholder: "Email",
-              controller: _emailField,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 16.0, right: 16.0),
-            child: RegisterInput(
-              placeholder: "Contraseña",
-              controller: _passwordField,
-              obscureText: true,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 16.0, right: 16.0),
-            child: RegisterInput(
-              placeholder: "Repetir contraseña",
-              controller: _passwordField2,
-              obscureText: true,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 16.0, right: 16.0),
-            child: TextButton(
-              child: SizedBox(
-                  width: double.infinity,
-                  child: Text("Registrarse",
-                      style: TextStyle(fontSize: 20),
-                      textAlign: TextAlign.center)),
-              style: ButtonStyle(
-                  backgroundColor:
-                      MaterialStateProperty.all<Color>(MyTheme.white),
-                  padding:
-                      MaterialStateProperty.all<EdgeInsets>(EdgeInsets.all(15)),
-                  foregroundColor:
-                      MaterialStateProperty.all<Color>(MyTheme.black),
-                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                      RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(18.0),
-                          side: BorderSide(color: MyTheme.black, width: 1.1)))),
-              onPressed: () async {
-                bool shouldNavigate =
-                    await register(_emailField.text, _passwordField.text);
-                if (shouldNavigate) {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => Home(),
-                    ),
-                  );
-                }
-              },
-            ),
-          )
-        ]),
-      )
-
-          /*Container(
-          alignment: Alignment.center,
-          width: MediaQuery.of(context).size.width / 1.5,
-          height: MediaQuery.of(context).size.height / 1.5,
+        child: Container(
+          width: MediaQuery.of(context).size.width / 1.3,
+          height: MediaQuery.of(context).size.height / 1.2,
+          margin: const EdgeInsets.all(10.0),
           decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
             color: MyTheme.primary,
           ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              TextFormField(
+          child:
+              Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+            Container(
+              child: Center(
+                child: Image.asset("assets/img/rentItLogo.png",
+                    height: 200, width: 200),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 16.0, right: 16.0),
+              child: RegisterInput(
+                placeholder: "Usuario",
                 controller: _userField,
-                decoration: InputDecoration(
-                    hintText: "ejemplo",
-                    hintStyle: TextStyle(
-                      color: Colors.white,
-                    ),
-                    labelText: "Usuario",
-                    labelStyle: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                    )),
               ),
-              TextFormField(
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 16.0, right: 16.0),
+              child: RegisterInput(
+                placeholder: "Email",
                 controller: _emailField,
-                decoration: InputDecoration(
-                    hintText: "ejemplo@email.com",
-                    hintStyle: TextStyle(
-                      color: Colors.white,
-                    ),
-                    labelText: "Email",
-                    labelStyle: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                    )),
               ),
-              TextFormField(
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 16.0, right: 16.0),
+              child: RegisterInput(
+                placeholder: "Contraseña",
                 controller: _passwordField,
                 obscureText: true,
-                decoration: InputDecoration(
-                    hintText: "contraseña",
-                    hintStyle: TextStyle(
-                      color: Colors.white,
-                    ),
-                    labelText: "Contraseña",
-                    labelStyle: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                    )),
               ),
-              TextFormField(
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 16.0, right: 16.0),
+              child: RegisterInput(
+                placeholder: "Repetir contraseña",
                 controller: _passwordField2,
                 obscureText: true,
-                decoration: InputDecoration(
-                    hintText: "repetir contraseña",
-                    hintStyle: TextStyle(
-                      color: Colors.white,
-                    ),
-                    labelText: "Repetir contraseña",
-                    labelStyle: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                    )),
               ),
-              TextButton(
-                  child: SizedBox(
-                      width: double.infinity,
-                      child: Text("Registrarse",
-                          style: TextStyle(fontSize: 20),
-                          textAlign: TextAlign.center)),
-                  style: ButtonStyle(
-                      backgroundColor:
-                          MaterialStateProperty.all<Color>(MyTheme.white),
-                      padding: MaterialStateProperty.all<EdgeInsets>(
-                          EdgeInsets.all(15)),
-                      foregroundColor:
-                          MaterialStateProperty.all<Color>(MyTheme.black),
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                          RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(18.0),
-                              side: BorderSide(
-                                  color: MyTheme.black, width: 1.1)))),
-                  onPressed: () => null),
-            ],
-          )
-       ),*/
-          ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 16.0, right: 16.0),
+              child: TextButton(
+                child: SizedBox(
+                    width: double.infinity,
+                    child: Text("Registrarse",
+                        style: TextStyle(fontSize: 20),
+                        textAlign: TextAlign.center)),
+                style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(MyTheme.white),
+                    padding:
+                        MaterialStateProperty.all<EdgeInsets>(EdgeInsets.all(15)),
+                    foregroundColor:
+                        MaterialStateProperty.all<Color>(MyTheme.black),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(18.0),
+                            side: BorderSide(color: MyTheme.black, width: 1.1)))),
+                onPressed: () async {
+                  bool shouldNavigate =
+                      await register(_emailField.text, _passwordField.text);
+                  if (shouldNavigate) {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => Home(),
+                      ),
+                    );
+                  }
+                },
+              ),
+            )
+          ]),
+        )
+      ),
     );
   }
 }
+
+ */
