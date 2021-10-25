bcf0dd9b08458e8dc1c3ccdb0e577d289d371200
Juan <jdeluca@itba.edu.ar>
2021-10-24 21:47:22 -0300

    continue profile

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -184,19 +184,21 @@ class Profile extends StatelessWidget {
                                         ),
                                         SizedBox(height: 10.0),
                                         Align(
-                                          child: Row(
-                                            mainAxisAlignment: MainAxisAlignment.center,
-                                            children: [
+                                            child: Row(
+                                                mainAxisAlignment:
+                                                    MainAxisAlignment.center,
+                                                children: [
                                               IconButton(
-                                                icon: Icon(Icons.settings,
-                                                    color: MyTheme.muted,
-                                                    size: 22.0),
-                                                onPressed: null),
+                                                  icon: Icon(Icons.settings,
+                                                      color: MyTheme.muted,
+                                                      size: 22.0),
+                                                  onPressed: null),
                                               IconButton(
                                                   icon: Icon(Icons.edit,
                                                       color: MyTheme.muted,
                                                       size: 22.0),
-                                                  onPressed: null)])),
+                                                  onPressed: null)
+                                            ])),
                                         // Align(
                                         //   child: Text("San Francisco, USA",
                                         //       style: TextStyle(
@@ -243,6 +245,106 @@ class Profile extends StatelessWidget {
                                             children: [
                                               Text(
                                                 "Publicaciones",
+                                                style: TextStyle(
+                                                    fontWeight: FontWeight.bold,
+                                                    fontSize: 16.0,
+                                                    color: MyTheme.text),
+                                              ),
+                                              Text(
+                                                "Ver todas",
+                                                style: TextStyle(
+                                                    color: MyTheme.primary,
+                                                    fontSize: 13.0,
+                                                    fontWeight:
+                                                        FontWeight.w600),
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                        SizedBox(
+                                          height: 250,
+                                          child: GridView.count(
+                                              primary: false,
+                                              padding: EdgeInsets.symmetric(
+                                                  horizontal: 24.0,
+                                                  vertical: 15.0),
+                                              crossAxisSpacing: 10,
+                                              mainAxisSpacing: 10,
+                                              crossAxisCount: 3,
+                                              children: <Widget>[
+                                                Container(
+                                                    height: 100,
+                                                    decoration: BoxDecoration(
+                                                      borderRadius:
+                                                          BorderRadius.all(
+                                                              Radius.circular(
+                                                                  6.0)),
+                                                      image: DecorationImage(
+                                                          image: NetworkImage(
+                                                              "https://images.unsplash.com/photo-1501601983405-7c7cabaa1581?fit=crop&w=240&q=80"),
+                                                          fit: BoxFit.cover),
+                                                    )),
+                                                Container(
+                                                    decoration: BoxDecoration(
+                                                  borderRadius:
+                                                      BorderRadius.all(
+                                                          Radius.circular(6.0)),
+                                                  image: DecorationImage(
+                                                      image: NetworkImage(
+                                                          "https://images.unsplash.com/photo-1543747579-795b9c2c3ada?fit=crop&w=240&q=80hoto-1501601983405-7c7cabaa1581?fit=crop&w=240&q=80"),
+                                                      fit: BoxFit.cover),
+                                                )),
+                                                Container(
+                                                    decoration: BoxDecoration(
+                                                  borderRadius:
+                                                      BorderRadius.all(
+                                                          Radius.circular(6.0)),
+                                                  image: DecorationImage(
+                                                      image: NetworkImage(
+                                                          "https://images.unsplash.com/photo-1551798507-629020c81463?fit=crop&w=240&q=80"),
+                                                      fit: BoxFit.cover),
+                                                )),
+                                                Container(
+                                                    decoration: BoxDecoration(
+                                                  borderRadius:
+                                                      BorderRadius.all(
+                                                          Radius.circular(6.0)),
+                                                  image: DecorationImage(
+                                                      image: NetworkImage(
+                                                          "https://images.unsplash.com/photo-1470225620780-dba8ba36b745?fit=crop&w=240&q=80"),
+                                                      fit: BoxFit.cover),
+                                                )),
+                                                Container(
+                                                    decoration: BoxDecoration(
+                                                  borderRadius:
+                                                      BorderRadius.all(
+                                                          Radius.circular(6.0)),
+                                                  image: DecorationImage(
+                                                      image: NetworkImage(
+                                                          "https://images.unsplash.com/photo-1503642551022-c011aafb3c88?fit=crop&w=240&q=80"),
+                                                      fit: BoxFit.cover),
+                                                )),
+                                                Container(
+                                                    decoration: BoxDecoration(
+                                                  borderRadius:
+                                                      BorderRadius.all(
+                                                          Radius.circular(6.0)),
+                                                  image: DecorationImage(
+                                                      image: NetworkImage(
+                                                          "https://images.unsplash.com/photo-1482686115713-0fbcaced6e28?fit=crop&w=240&q=80"),
+                                                      fit: BoxFit.cover),
+                                                )),
+                                              ]),
+                                        ),
+                                        Padding(
+                                          padding: const EdgeInsets.only(
+                                              right: 25.0, left: 25.0),
+                                          child: Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.spaceBetween,
+                                            children: [
+                                              Text(
+                                                "Alquileres pasados",
                                                 style: TextStyle(
                                                     fontWeight: FontWeight.bold,
                                                     fontSize: 16.0,
