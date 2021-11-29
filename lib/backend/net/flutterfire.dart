3b6cd2522f914f8d866b375facdc0e3151755888
Santiago Sandrini <ssandrini@itba.edu.ar>
2021-11-28 23:25:04 -0300

    avances en back

--- lib/backend/net/flutterfire.dart
+++ lib/backend/net/flutterfire.dart
@@ -1,9 +1,13 @@
+import 'package:argon_flutter/backend/models/product-model.dart';
+import 'package:argon_flutter/backend/models/publication-model.dart';
 import 'package:argon_flutter/backend/models/user-model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 final fireStoreInstance = FirebaseFirestore.instance;
 
+
+/// ****************************************USERS*********************************************************/
 Future<bool> signIn(String email, String password) async {
   try {
     await FirebaseAuth.instance
@@ -33,34 +37,11 @@ Future<bool> register(String email, String password) async {
   }
 }
 
-Future<bool> addProduct(String name, String detail, String category) async {
-  try {
-    String uid = FirebaseAuth.instance.currentUser.uid;
-    CollectionReference collectionReference = FirebaseFirestore.instance
-        .collection('Users')
-        .doc(uid)
-        .collection('Products');
-
-    await collectionReference.add({
-      "name": name,
-      "detail": detail,
-      "category": category,
-    });
-    return true;
-  } catch (e) {
-    return false;
-  }
-}
 
 Future<bool> addInformation(String firstName, String lastName, String age) async {
-  /* me gustaria mejorar al metodo para que puedas actualizar sólo algunos campos
-  y no necesariamente todos.
-   */
   try {
     User _user = FirebaseAuth.instance.currentUser;
-    /* el doc "info" deberia ser único, todavia no probe si puedo
-    usar la palabra "info" como id.
-     */
+
     DocumentReference documentReference = FirebaseFirestore.instance
         .collection('Users')
         .doc(_user.uid)
@@ -88,6 +69,31 @@ Future<bool> addInformation(String firstName, String lastName, String age) async
   }
 }
 
+
+/// ******************************************************************************************************/
+
+
+
+/// ************************************PRODUCTS /  PUBLICATIONS******************************************/
+///
+Future<bool> addProduct(String name, String detail, String category) async {
+  try {
+    String uid = FirebaseAuth.instance.currentUser.uid;
+    CollectionReference collectionReference = FirebaseFirestore.instance
+        .collection('Users')
+        .doc(uid)
+        .collection('Products');
+    ProductModel productModel = ProductModel();
+    productModel.name = name;
+    productModel.detail = detail;
+    productModel.category = category;
+    await collectionReference.add(productModel.toMap());
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+
 Future<bool> removeProduct(String id) async {
   String uid = FirebaseAuth.instance.currentUser.uid;
   FirebaseFirestore.instance
@@ -98,3 +104,29 @@ Future<bool> removeProduct(String id) async {
       .delete();
   return true;
 }
+
+Future<bool> addPublication(String name, ProductModel productModel, double price) async {
+  try {
+    String uid = FirebaseAuth.instance.currentUser.uid;
+    CollectionReference collectionReference = FirebaseFirestore.instance
+        .collection('Users')
+        .doc(uid)
+        .collection('Publications');
+
+    PublicationModel publicationModel = PublicationModel();
+    publicationModel.name = name;
+    publicationModel.productModel = productModel;
+    publicationModel.price = price;
+    await collectionReference.add(publicationModel.toMap());
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+
+// falta el delete de publications
+
+
+/// ******************************************************************************************************/
+///
+/// ****************************************** ORDERS ****************************************************/
