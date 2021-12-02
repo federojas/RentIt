4dce5e4393b154557ea75281dc592e2c4f900aac
Juan <jdeluca@itba.edu.ar>
2021-12-02 01:10:42 -0300

    Auto stash before merge of "main" and "origin/main"

--- lib/screens/categoryList.dart
+++ lib/screens/categoryList.dart
@@ -30,16 +30,11 @@ class _CategoryListState extends State<CategoryList> {
   }
 
   Widget getListView() {
-    PublicationModel pm = PublicationModel();
-    pm.name = "name";
-    pm.uid = "uid";
-    pm.detail = "detail";
-    pm.category = "category";
-    pm.price = "price";
-    pm.images = null;
     return FutureBuilder(
       builder: (context, snapshot) {
-        if (snapshot.hasData && snapshot.data != null && snapshot.data.length != 0) {
+        if (snapshot.hasData &&
+            snapshot.data != null &&
+            snapshot.data.length != 0) {
           return Container(
               padding: const EdgeInsets.only(left: 10.0, right: 10.0),
               height: MediaQuery.of(context).size.height,
@@ -47,81 +42,73 @@ class _CategoryListState extends State<CategoryList> {
                 itemCount: snapshot.data == null ? 0 : snapshot.data.length,
                 itemBuilder: (context, index) {
                   PublicationModel pm = snapshot.data[index];
-                  return Column(children:[
+                  return Column(children: [
                     ListTile(
-                      onTap: () {Navigator.push(context,MaterialPageRoute(builder: (context) => ListingScreen(pm)));},
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => ListingScreen(pm)));
+                      },
                       leading: ConstrainedBox(
                         constraints: BoxConstraints(
-                            minWidth: MediaQuery.of(context).size.width/2.5,
-                        ),
-                        child: Image.network(pm.images[0], fit: BoxFit.cover),
+                            minWidth: MediaQuery.of(context).size.width / 2.5),
+                        child: Image.network(pm.images[0],
+                            fit: BoxFit.cover, height: 70),
                       ),
                       title: Text(pm.name),
-                      subtitle: Row(children:[Text("\$ ", style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20, color: MyTheme.black)),
-                                    Text(pm.price, style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20, color: MyTheme.black)),
-                                    Text("\\", style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20, color: MyTheme.black)),
-                                    Text(pm.timeUnit, style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20, color: MyTheme.black))]),
+                      subtitle: Row(children: [
+                        Text("\$ ",
+                            style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                                color: MyTheme.black)),
+                        Text(pm.price,
+                            style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                                color: MyTheme.black)),
+                        Text("\\",
+                            style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                                color: MyTheme.black)),
+                        Text(pm.timeUnit,
+                            style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                                color: MyTheme.black))
+                      ]),
                     ),
-                    Divider(thickness: 0.5,
-                        color: Colors.grey),
+                    Divider(thickness: 0.5, color: Colors.grey),
                   ]);
                 },
-              )
-          );
-        } else if( snapshot.connectionState == ConnectionState.waiting){
+              ));
+        } else if (snapshot.connectionState == ConnectionState.waiting) {
           return Align(
-              alignment: Alignment.center,
-            child: Container(
-            child: CircularProgressIndicator()
-          ),);
+            alignment: Alignment.center,
+            child: Container(child: CircularProgressIndicator()),
+          );
         } else {
           return Align(
-              alignment: Alignment.center,
+            alignment: Alignment.center,
             child: Container(
               child: Text(
                 "No hay publicaciones",
                 textAlign: TextAlign.center,
                 overflow: TextOverflow.ellipsis,
-                style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                style:
+                    const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
               ),
-          ),
+            ),
           );
         }
       },
       future: getPublicationsByCategory(category),
     );
-
-    /*
-    return Container(
-        padding: const EdgeInsets.only(left: 24.0, right: 24.0, top: 16.0),
-        child: ListView(
-          children: [
-            Text(
-              category,
-              textAlign: TextAlign.center,
-              overflow: TextOverflow.ellipsis,
-              style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-            ),
-            FutureBuilder(
-                future: getPublicationsByCategory(category),
-                builder: (context, snapshot) {
-                  if (snapshot.hasData) {
-                    PublicationModel pm = snapshot.data[0];
-                    return ListTile(
-                      leading: CircleAvatar(
-                          backgroundImage: NetworkImage(pm.images[0]),
-                          radius: 25.0),
-                      title: Text(pm.name),
-                      subtitle: Text(pm.price),
-                      trailing: Icon(Icons.favorite_border),
-                    );
-                  } else {
-                    return CircularProgressIndicator();
-                  }
-                }),
-          ],
-        ));
-
-     */
   }
+
+  // Widget getListTile(PublicationModel pm) {
+  //   return
+  // }
 }
