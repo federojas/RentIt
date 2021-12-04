e01c1d0bc9fc3f48187eb4fc247c713397c00f77
Santiago Sandrini <ssandrini@itba.edu.ar>
2021-12-04 11:32:52 -0300

    navbar fix

--- lib/widgets/navbar.dart
+++ lib/widgets/navbar.dart
@@ -71,15 +71,7 @@ class _NavbarState extends State<Navbar> {
     bool fav = false;
 
     return Container(
-      height: (MediaQuery.of(context).size.height)/8,
-        width: (MediaQuery.of(context).size.width),
-        // height: widget.searchBar
-        //     ? (!categories
-        //         ? (tagsExist ? 211.0 : 178.0)
-        //         : (tagsExist ? 262.0 : 210.0))
-        //     : (!categories
-        //         ? (tagsExist ? 162.0 : 102.0)
-        //         : (tagsExist ? 200.0 : 150.0)),
+        height: 150.0,
         decoration: BoxDecoration(
             color: !widget.transparent ? widget.bgColor : Colors.transparent,
             boxShadow: [
@@ -92,173 +84,178 @@ class _NavbarState extends State<Navbar> {
                   offset: Offset(0, 5))
             ]),
         child: SafeArea(
-          child: Container(
-            padding: const EdgeInsets.all(25.0),
-            child: Row(
+          child: Padding(
+            padding: const EdgeInsets.only(left: 16.0, right: 16.0),
+            child: Column(
+              children: [
+                Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisSize: MainAxisSize.max,
                   children: [
-                    if(widget.backButton)
-                        IconButton(
-                            icon: Icon(Icons.arrow_back_ios,
-                                size: 30.0, color: MyTheme.white),
-                            onPressed: () {
-                                Navigator.pop(context);
-                            }),
+                    Row(
+                      children: [
 
-                    if(widget.searchBar && !widget.backButton)
-                      Container(
-                          height: (MediaQuery.of(context).size.height),
-
-                          width: (MediaQuery.of(context).size.width)/1.15,
-                          child: Input(
-                                    placeholder: "¿Qué buscás?",
-                                    controller: widget.searchController,
-                                    onChanged: widget.searchOnChanged,
-                                    autofocus: widget.searchAutofocus,
-                                    suffixIcon: Icon(Icons.zoom_in, color: MyTheme.muted),
-                                    onTap: () => null
-                                ),
-                      ),
-                    if(widget.searchBar && widget.backButton)
-                      Container(
-                        height: (MediaQuery.of(context).size.height),
-
-                        width: (MediaQuery.of(context).size.width)/1.75,
-                        child: Input(
-                            placeholder: "¿Qué buscás?",
-                            controller: widget.searchController,
-                            onChanged: widget.searchOnChanged,
-                            autofocus: widget.searchAutofocus,
-                            suffixIcon: Icon(Icons.zoom_in, color: MyTheme.muted),
-                            onTap: () => null
-                        ),
-                      ),
-
-                    if(!widget.searchBar)
-                      Text(widget.title,
+                        Text(widget.title,
                             style: TextStyle(
                                 color: !widget.transparent
                                     ? (widget.bgColor == MyTheme.white
-                                        ? MyTheme.initial
-                                        : MyTheme.white)
+                                    ? MyTheme.initial
+                                    : MyTheme.white)
                                     : MyTheme.white,
                                 fontWeight: FontWeight.w600,
-                                fontSize: 20.0)),
-                    Row( mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        crossAxisAlignment: CrossAxisAlignment.center,
+                                fontSize: 18.0)),
+                      ],
+                    ),
+                    if (widget.rightOptions)
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         children: [
-                          if(widget.favOption)
-                            IconButton(
-                                icon: fav
-                                    ? Icon(Icons.favorite, color: !widget.transparent
-                                    ? (widget.bgColor == MyTheme.white
-                                    ? MyTheme.initial
-                                    : MyTheme.white)
-                                    : MyTheme.white, size: 26.0)
-                                    : Icon(Icons.favorite_border, color: !widget.transparent
-                                    ? (widget.bgColor == MyTheme.white
-                                    ? MyTheme.initial
-                                    : MyTheme.white)
-                                    : MyTheme.white, size: 26.0),
-                                onPressed: () {
-                                  setState(() {
-                                    fav = !fav;
-                                  });
-                                }),
-                          ])
-
-
-                // if (categories)
-                //   Row(
-                //     mainAxisAlignment: MainAxisAlignment.center,
-                //     crossAxisAlignment: CrossAxisAlignment.center,
-                //     children: [
-                //       GestureDetector(
-                //         onTap: () => null,
-                //         child: Row(
-                //           children: [
-                //             Icon(Icons.camera,
-                //                 color: MyTheme.initial, size: 22.0),
-                //             SizedBox(width: 10),
-                //             Text(widget.categoryOne,
-                //                 style: TextStyle(
-                //                     color: MyTheme.initial, fontSize: 16.0)),
-                //           ],
-                //         ),
-                //       ),
-                //       SizedBox(width: 30),
-                //       Container(
-                //         color: MyTheme.initial,
-                //         height: 25,
-                //         width: 1,
-                //       ),
-                //       SizedBox(width: 30),
-                //       GestureDetector(
-                //         onTap: () => null,
-                //         child: Row(
-                //           children: [
-                //             Icon(Icons.shopping_cart,
-                //                 color: MyTheme.initial, size: 22.0),
-                //             SizedBox(width: 10),
-                //             Text(widget.categoryTwo,
-                //                 style: TextStyle(
-                //                     color: MyTheme.initial, fontSize: 16.0)),
-                //           ],
-                //         ),
-                //       )
-                //     ],
-                //   ),
-                // if (tagsExist)
-                //   Container(
-                //     height: 40,
-                //     child: ScrollablePositionedList.builder(
-                //       itemScrollController: _scrollController,
-                //       scrollDirection: Axis.horizontal,
-                //       itemCount: widget.tags.length,
-                //       itemBuilder: (BuildContext context, int index) {
-                //         return GestureDetector(
-                //           onTap: () {
-                //             if (activeTag != widget.tags[index]) {
-                //               setState(() => activeTag = widget.tags[index]);
-                //               _scrollController.scrollTo(
-                //                   index:
-                //                       index == widget.tags.length - 1 ? 1 : 0,
-                //                   duration: Duration(milliseconds: 420),
-                //                   curve: Curves.easeIn);
-                //               if (widget.getCurrentPage != null)
-                //                 widget.getCurrentPage(activeTag);
-                //             }
-                //           },
-                //           child: Container(
-                //               margin: EdgeInsets.only(
-                //                   left: index == 0 ? 46 : 8, right: 8),
-                //               padding: EdgeInsets.only(
-                //                   top: 4, bottom: 4, left: 20, right: 20),
-                //               // width: 90,
-                //               decoration: BoxDecoration(
-                //                   color: activeTag == widget.tags[index]
-                //                       ? MyTheme.primary
-                //                       : MyTheme.secondary,
-                //                   borderRadius:
-                //                       BorderRadius.all(Radius.circular(4.0))),
-                //               child: Center(
-                //                 child: Text(widget.tags[index],
-                //                     style: TextStyle(
-                //                         color: activeTag == widget.tags[index]
-                //                             ? MyTheme.white
-                //                             : MyTheme.black,
-                //                         fontWeight: FontWeight.w600,
-                //                         fontSize: 14.0)),
-                //               )),
-                //         );
-                //       },
-                //     ),
-                //   )
+                          GestureDetector(
+                            onTap: () {
+                              ;
+                            },
+                            child: IconButton(
+                                icon: Icon(Icons.notifications_active,
+                                    color: !widget.transparent
+                                        ? (widget.bgColor == MyTheme.white
+                                        ? MyTheme.initial
+                                        : MyTheme.white)
+                                        : MyTheme.white,
+                                    size: 22.0),
+                                onPressed: null),
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              ;
+                            },
+                            child: IconButton(
+                                icon: Icon(Icons.shopping_basket,
+                                    color: !widget.transparent
+                                        ? (widget.bgColor == MyTheme.white
+                                        ? MyTheme.initial
+                                        : MyTheme.white)
+                                        : MyTheme.white,
+                                    size: 22.0),
+                                onPressed: null),
+                          ),
+                        ],
+                      )
+                  ],
+                ),
+                if (widget.searchBar)
+                  Padding(
+                    padding: const EdgeInsets.only(
+                        top: 8, bottom: 4, left: 15, right: 15),
+                    child: Input(
+                        placeholder: "  ¿Qué estás buscando?",
+                        controller: widget.searchController,
+                        onChanged: widget.searchOnChanged,
+                        autofocus: widget.searchAutofocus,
+                        suffixIcon:
+                        Icon(Icons.zoom_in, color: MyTheme.muted),
+                        onTap: () {
+                          // ?
+                        }),
+                  ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                if (categories)
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          ;
+                        },
+                        child: Row(
+                          children: [
+                            Icon(Icons.camera,
+                                color: MyTheme.initial, size: 22.0),
+                            SizedBox(width: 10),
+                            Text(widget.categoryOne,
+                                style: TextStyle(
+                                    color: MyTheme.initial,
+                                    fontSize: 16.0)),
+                          ],
+                        ),
+                      ),
+                      SizedBox(width: 30),
+                      Container(
+                        color: MyTheme.initial,
+                        height: 25,
+                        width: 1,
+                      ),
+                      SizedBox(width: 30),
+                      GestureDetector(
+                        onTap: () {
+                          ;
+                        },
+                        child: Row(
+                          children: [
+                            Icon(Icons.shopping_cart,
+                                color: MyTheme.initial, size: 22.0),
+                            SizedBox(width: 10),
+                            Text(widget.categoryTwo,
+                                style: TextStyle(
+                                    color: MyTheme.initial,
+                                    fontSize: 16.0)),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                if (tagsExist)
+                  Container(
+                    height: 40,
+                    child: ScrollablePositionedList.builder(
+                      itemScrollController: _scrollController,
+                      scrollDirection: Axis.horizontal,
+                      itemCount: widget.tags.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return GestureDetector(
+                          onTap: () {
+                            if (activeTag != widget.tags[index]) {
+                              setState(() => activeTag = widget.tags[index]);
+                              _scrollController.scrollTo(
+                                  index:
+                                  index == widget.tags.length - 1 ? 1 : 0,
+                                  duration: Duration(milliseconds: 420),
+                                  curve: Curves.easeIn);
+                              if (widget.getCurrentPage != null)
+                                widget.getCurrentPage(activeTag);
+                            }
+                          },
+                          child: Container(
+                              margin: EdgeInsets.only(
+                                  left: index == 0 ? 46 : 8, right: 8),
+                              padding: EdgeInsets.only(
+                                  top: 4, bottom: 4, left: 20, right: 20),
+                              // width: 90,
+                              decoration: BoxDecoration(
+                                  color: activeTag == widget.tags[index]
+                                      ? MyTheme.primary
+                                      : MyTheme.secondary,
+                                  borderRadius:
+                                  BorderRadius.all(Radius.circular(4.0))),
+                              child: Center(
+                                child: Text(widget.tags[index],
+                                    style: TextStyle(
+                                        color: activeTag == widget.tags[index]
+                                            ? MyTheme.white
+                                            : MyTheme.black,
+                                        fontWeight: FontWeight.w600,
+                                        fontSize: 14.0)),
+                              )),
+                        );
+                      },
+                    ),
+                  )
               ],
             ),
           ),
-        ));
+        )
+    );
   }
 }
